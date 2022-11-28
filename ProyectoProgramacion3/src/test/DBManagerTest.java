6a3519e3828e906f40244dac494dd794c9ac2e2b
Iker Perez <ikperez@opendeusto.es>
2022-11-28 14:43:42 +0100

    Test del manger de la base de datos

--- ProyectoProgramacion3/src/test/DBManagerTest.java
+++ ProyectoProgramacion3/src/test/DBManagerTest.java
@@ -13,70 +13,64 @@ import baseDatos.DBManager;
 import clases.Usuario;
 import clases.UsuarioPublico;
 
-
 public class DBManagerTest {
 
-private DBManager con;
-private DBManager discon;
-private DBManager usuario;
+	private DBManager con;
+	private DBManager discon;
+	private DBManager usuario;
 
+	@Before
+	public void setUp() throws Exception {
+		con.conectar();
+	}
 
-@Before
-public void setUp() throws Exception {
-con.conectar();
-}
+	@After
+	public void tearDown() throws Exception {
+		discon.disconnect();
+	}
 
-@After
-public void tearDown() throws Exception {
-discon.disconnect();
-}
+	@Test // para asegurarnos de que no da error al crear lista
+	public void testCrearLista() {
 
-@Test //para asegurarnos de que no da error al crear lista
-public void testCrearLista() {
+		try {
 
-try {
+			usuario.crearLista();
 
-usuario.crearLista();
-
-} catch (Exception e) {
+		} catch (Exception e) {
 // TODO: handle exception
 
-fail( "Error a la hora de crear lista" );
+			fail("Error a la hora de crear lista");
 
-}
+		}
 
-}
+	}
 
-@Test        //para ver que cuando añadimos un usuario, el tamaño del array incrementa
-public void testActualizarUsuarios() {
+	@Test // para ver que cuando añadimos un usuario, el tamaño del array incrementa
+	public void testActualizarUsuarios() {
 
-List<UsuarioPublico> up = new ArrayList<>();
+		List<UsuarioPublico> up = new ArrayList<>();
 
-up=usuario.crearLista();
-int largoLista= up.size();
+		up = usuario.crearLista();
+		int largoLista = up.size();
 
-usuario.actualizarUsuarios(null);
+		usuario.actualizarUsuarios(null);
 
-assertEquals(largoLista+1, usuario.crearLista().size());
+		assertEquals(largoLista + 1, usuario.crearLista().size());
 
-}
+	}
 
-@Test     //para ver que cuando eliminamos un usuario, el tamaño del array disminuye
-public void testEliminarUsuario() {
+	@Test // para ver que cuando eliminamos un usuario, el tamaño del array disminuye
+	public void testEliminarUsuario() {
 
-List<UsuarioPublico> up = new ArrayList<>();
+		List<UsuarioPublico> up = new ArrayList<>();
 
-up=usuario.crearLista();
-int largoLista= up.size();
+		up = usuario.crearLista();
+		int largoLista = up.size();
 
-usuario.eliminarUsuario(null);
+		usuario.eliminarUsuario(null);
 
-assertEquals(largoLista-1, usuario.crearLista().size());
+		assertEquals(largoLista - 1, usuario.crearLista().size());
 
-
-
-}
-
-
+	}
 
 }
