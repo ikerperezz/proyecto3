7b2ba62294eb6f65f95069458abe3d6f2ad580c7
Iker Perez <ikperez@opendeusto.es>
2022-11-29 08:35:01 +0100

    Corregido Usuario Publico Test

--- ProyectoProgramacion3/src/test/UsuarioPublicoTest.java
+++ ProyectoProgramacion3/src/test/UsuarioPublicoTest.java
@@ -11,34 +11,34 @@ public class UsuarioPublicoTest {
 
 	@Test
 	public void testGetIdLiga() {
-		UsuarioPublico u = new UsuarioPublico("jigoa", "123j", 001, "a1",1000000);
+		UsuarioPublico u = new UsuarioPublico("jigoa", "123j", 001, 1,1000000);
 		assertEquals("a1", u.getIdLiga());
 	}
 	@Test
 	public void testGetDineroDisponible() {
-		UsuarioPublico u = new UsuarioPublico("jigoa", "123j", 001, "a1",1000000);
+		UsuarioPublico u = new UsuarioPublico("jigoa", "123j", 001, 1,1000000);
 		assertEquals(1000000, u.getDineroDisponible());
 	}
 	@Test
 	public void testSetIdLiga() {
-		UsuarioPublico u = new UsuarioPublico("jigoa", "123j", 001, "a1",1000000);
-		u.setIdLiga("a2");
-		assertEquals("a2", u.getIdLiga());
+		UsuarioPublico u = new UsuarioPublico("jigoa", "123j", 001, 1,1000000);
+		u.setIdLiga(2);
+		assertEquals(2, u.getIdLiga());
 	}
 	@Test
 	public void testSetDineroDisponible() {
-		UsuarioPublico u = new UsuarioPublico("jigoa", "123j", 001, "a1",1000000);
+		UsuarioPublico u = new UsuarioPublico("jigoa", "123j", 001, 1,1000000);
 		u.setDineroDisponible(99000000);
 		assertEquals(99000000, u.getDineroDisponible());
 	}
 	@Test
 	public void testGetIdUsuario() {
-		UsuarioPublico u = new UsuarioPublico("jigoa", "123j", 001, "a1",1000000);
+		UsuarioPublico u = new UsuarioPublico("jigoa", "123j", 001, 1,1000000);
 		assertEquals("001", u.getIdUsuarioPublico());
 	}
 	@Test
 	public void testSetIdUsuario() {
-		UsuarioPublico u = new UsuarioPublico("jigoa", "123j", 001, "a1",1000000);
+		UsuarioPublico u = new UsuarioPublico("jigoa", "123j", 001, 1,1000000);
 		u.setIdUsuarioPublico(002);
 		assertEquals(002, u.getDineroDisponible());
 	}
