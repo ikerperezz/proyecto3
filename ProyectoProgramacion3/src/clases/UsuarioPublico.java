965d9fce21f8dcdf44352ace2552b74ab22a9fb6
ALUMNO <ALUMNO@DBS22-123456789>
2022-12-02 18:44:04 +0100

    Creado método estático para que funcione el método compare de
    la ventana de clasificación

--- ProyectoProgramacion3/src/clases/UsuarioPublico.java
+++ ProyectoProgramacion3/src/clases/UsuarioPublico.java
@@ -1,41 +1,41 @@
 package clases;
 
 public class UsuarioPublico extends Usuario {
-		private int idUsuarioPublico;
-		private int idLiga;
-		private int dineroDisponible;
-		private int puntos;
-	
-		
-	public int getPuntos() {
-			return puntos;
-		}
+	private int idUsuarioPublico;
+	private int idLiga;
+	private int dineroDisponible;
+	private static int puntos;
 
-		public void setPuntos(int puntos) {
-			this.puntos = puntos;
-		}
+	public int getPuntos() {
+		return puntos;
+	}
+
+	public void setPuntos(int puntos) {
+		this.puntos = puntos;
+	}
 
 	public int getIdLiga() {
-			return idLiga;
-		}
+		return idLiga;
+	}
 
-		public void setIdLiga(int idLiga) {
-			this.idLiga = idLiga;
-		}
+	public void setIdLiga(int idLiga) {
+		this.idLiga = idLiga;
+	}
 
-		public int getDineroDisponible() {
-			return dineroDisponible;
-		}
+	public int getDineroDisponible() {
+		return dineroDisponible;
+	}
 
-		public void setDineroDisponible(int dineroDisponible) {
-			this.dineroDisponible = dineroDisponible;
-		}
+	public void setDineroDisponible(int dineroDisponible) {
+		this.dineroDisponible = dineroDisponible;
+	}
 
-	public UsuarioPublico(String usuario, String contraseina, int idUsuarioPublico,int idLiga, int dineroDisponible, int puntos) {
+	public UsuarioPublico(String usuario, String contraseina, int idUsuarioPublico, int idLiga, int dineroDisponible,
+			int puntos) {
 		super(usuario, contraseina);
-		this.idUsuarioPublico=idUsuarioPublico;
-		this.idLiga=idLiga;
-		this.dineroDisponible=dineroDisponible;
+		this.idUsuarioPublico = idUsuarioPublico;
+		this.idLiga = idLiga;
+		this.dineroDisponible = dineroDisponible;
 	}
 
 	public int getIdUsuarioPublico() {
@@ -48,7 +48,18 @@ public class UsuarioPublico extends Usuario {
 
 	@Override
 	public String toString() {
-		return usuario + "-" + puntos ;
+		return usuario + "-" + puntos;
 	}
 
+	public static int compare(int puntos1, int puntos2) {
+		if(puntos1 > puntos2) {
+			return puntos1;
+		
+		}else {
+			if(puntos1 > puntos2) {
+				return puntos2;
+			}
+		}
+		return puntos;
+	}
 }
